4e2cdfa88575c68e2f4b42f5820aa09b91aa97ec
Harikrishnan <hkkrishn563@gmail.com>
2020-09-10 16:45:27 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+//Author:Harikrishnan Kuppusamykrishnan
+//Project: NodeHub
+//Date: 08/06/2020
+//Description: this is the root file of our node.js application that is run on the server
+
+
+//spin up node.js server
