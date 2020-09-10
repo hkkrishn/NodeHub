b57784bfb9b3a8c81347ccfbb0355b7918f6b28a
Harikrishnan <hkkrishn563@gmail.com>
2020-09-10 16:57:31 -0700

    Node.js req obj

--- app.js
+++ app.js
@@ -5,3 +5,26 @@
 
 
 //spin up node.js server
+
+//require either takes a path to another file or a core module
+// a path to  another local file file has to always to start with ./
+// without ./ it will look for it globally
+const http = require('http');
+
+//Request Listener, request call back function
+const rqListener=(req,res)=>{
+    //this function which will take in the req and res objects as args
+    console.log(req)
+
+}
+
+//createServer takes a request listener or a function that executes for every
+//incoming req
+//it can also be an anonymous function
+//Create server method RETURNS a server and needs to be saved in a new var
+
+ const server = http.createServer(rqListener);
+
+ //listen will keep the server running and listen for incoming requests
+
+ server.listen(3000);
