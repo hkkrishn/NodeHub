9c80a23e0ae7e3374ff312b0f227b6cae93a56f9
Harikrishnan <hkkrishn563@gmail.com>
2020-09-13 00:45:41 -0700

    LCD

---
+++ Arduino/LCD.js
@@ -0,0 +1,25 @@
+var five = require("johnny-five"),
+  board, lcd;
+
+board = new five.Board();
+
+board.on("ready", function() {
+
+  lcd = new five.LCD({
+    // LCD pin name  RS  EN  DB4 DB5 DB6 DB7
+    // Arduino pin # 7    8   9   10  11  12
+    pins: [7, 8, 9, 10, 11, 12],
+    backlight: 6,
+    rows: 2,
+    cols: 20
+
+
+    // Options:
+    // bitMode: 4 or 8, defaults to 4
+    // lines: number of lines, defaults to 2
+    // dots: matrix dimensions, defaults to "5x8"
+  });
+
+ lcd.clear();
+ lcd.print("Bleep bloop");
+});
