935a875acebcedc60805c06b4fca4e4fd5d1db92
Harikrishnan <hkkrishn563@gmail.com>
2020-09-16 15:08:53 -0700

    express.js refactoring

--- Node_Basics/app.js
+++ Node_Basics/app.js
@@ -5,14 +5,37 @@
 
 //---------
 
-const http = require('http')
 const express = require('express')
 
 
 //make express application
 const app = express();
 
+//add middleware
+//use accepts an array of req handlers
+
+//the function passed in will be applied to every incoming request
+//next is a function defined by express.js and has to be executed for the request to travel on to the next middleware
+app.use((req,res,next)=>{
+    console.log('In the middleware')
+    //next has to be called to send req to the next middleware
+    next()
+})
+
+//express DOES NOT send a default response
+app.use((req,res,next)=>{
+    console.log('In the middleware2')
+    //send allows us to send a response and allows us to create a body of type any
+    //there is no need to set the Header
+    res.send('<h1>Hello from Hari</h1>')
+})
+
 //app is a valid request handler
 //app sets a way in handling incoming requests
-const server = http.createServer(app);
-server.listen(3000)
+
+
+//the below line is equivalent to ;
+// const server = http.createServer(app);
+// server.listen(3000)
+// No need for the http core module
+app.listen(3000)
