d054259b8360def2a9f89d2aab14c4a87afba0ea
Harikrishnan <hkkrishn563@gmail.com>
2020-09-16 16:45:42 -0700

    filter paths and 404 error pages

--- Node_Basics/app.js
+++ Node_Basics/app.js
@@ -20,8 +20,17 @@ const app = express();
 //this function registers a middleware and call next but before
 
 //get routes from admin.js
-app.use(adminRoutes)
+
+//if our paths in our router paths start with the same prefix we can parse it out
+//only paths starting with /admin will go into adminRoutes
+//express will omit the /admin when handling the routes in admin.js
+app.use('/admin',adminRoutes)
 app.use(shopRoutes)
+
+//if we dont handle the request we need a catch all middleware after all routes
+app.use((req,res,next)=>{
+    res.status(404).send('<h1>Page Not Found</h1>')
+})
 
 
 
--- Node_Basics/routes/admin.js
+++ Node_Basics/routes/admin.js
@@ -22,16 +22,21 @@ const router = express.Router();
 //if you want a middleware to be applied to all req put it on top
 
 
+
+
+//the /admin part has been outsourced in app.js
+
 router.get('/add-product',(req,res,next)=>{
 
     //send allows us to send a response and allows us to create a body of type any
     //there is no need to set the Header
-    res.send('<form action = "/product" method = "POST" ><input type = "text" name = "title"></input><button type = "submit"> Add Product</button></form>')
+    res.send('<form action = "/admin/add-product" method = "POST" ><input type = "text" name = "title"></input><button type = "submit"> Add Product</button></form>')
 })
 
 //since /product does not clash with /add-product it wont clash and can be placed before or after
 //app.get allows us to filter for get reqs and app.posts for post requests
-router.post('/product',(req,res,next)=>{
+//the same path can be used if the methods differ
+router.post('/add-product',(req,res,next)=>{
     console.log(req.body)
     //express function to redirect page to path
     res.redirect('/')
